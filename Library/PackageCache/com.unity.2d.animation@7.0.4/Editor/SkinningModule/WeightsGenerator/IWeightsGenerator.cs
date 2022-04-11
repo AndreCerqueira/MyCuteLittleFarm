e3dfec3046afc5306737b2b920e4d133a126dd83
Andre Cerqueira <andrecerq16@gmail.com>
2022-04-11 09:42:43 +0100

    .

--- Library/PackageCache/com.unity.2d.animation@7.0.4/Editor/SkinningModule/WeightsGenerator/IWeightsGenerator.cs
+++
@@ -1,9 +0,0 @@
-using UnityEngine;
-
-namespace UnityEditor.U2D.Animation
-{
-    internal interface IWeightsGenerator
-    {
-        BoneWeight[] Calculate(string name, Vector2[] vertices, int[] indices, Edge[] edges, Vector2[] controlPoints, Edge[] bones, int[] pins);
-    }
-}
